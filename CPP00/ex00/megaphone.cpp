2ea483a32d34491f008681b0f89e786b489304a7
Corentin Papot <cpapot@z4r9p1.42lyon.fr>
2023-08-03 17:13:32 +0200

    ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 15:51:57 by cpapot            #+#    #+#             */
+/*   Updated: 2023/08/03 16:36:34 by cpapot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+char	*ToUpperCase(char *str)
+{
+	int	index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 97 && str[index] <= 122)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+
+}
+
+int	main(int argc, char **argv)
+{
+	int	index = 1;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (index != argc)
+		{
+			std::cout << ToUpperCase(argv[index]) << std::endl;
+			index++;
+		}
+	}
+}
