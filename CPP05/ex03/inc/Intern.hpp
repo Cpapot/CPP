12619ff2c7adc95b6ada8bd3943a770acbcc2a1c
Cpapot <corentinpapot2004@gmail.com>
2023-12-18 15:44:29 +0100

    remove container in ex03

--- CPP05/ex03/inc/Intern.hpp
+++ CPP05/ex03/inc/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 19:19:54 by cpapot            #+#    #+#             */
-/*   Updated: 2023/12/13 17:05:57 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/12/18 15:41:23 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ public:
 	~Intern();
 
 	Intern const	&operator=(const Intern &src);
-	void	deleteForm(std::map<std::string, AForm*> formMap, std::string formName);
+	void	deleteForm(AForm *formArray[3], std::string formName);
 };
 
 #endif
--- CPP05/ex03/srcs/Intern.cpp
+++ CPP05/ex03/srcs/Intern.cpp
@@ -6,13 +6,13 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 09:27:44 by cpapot            #+#    #+#             */
-/*   Updated: 2023/12/13 17:06:36 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/12/18 15:42:48 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 
-void	Intern::deleteForm(std::map<std::string, AForm*> formMap, std::string formName)
+void	Intern::deleteForm(AForm *formArray[3], std::string formName)
 {
 	std::string	strArray[] =
 	{	"robotomy request",
@@ -22,25 +22,31 @@ void	Intern::deleteForm(std::map<std::string, AForm*> formMap, std::string formN
 	for (int i = 0; i != 3;i++)
 	{
 		if (strArray[i] != formName)
-			delete (formMap[strArray[i]]);
+			delete (formArray[i]);
 	}
 }
 
 AForm*	Intern::makeForm(std::string formName, std::string target)
 {
-	std::map<std::string, AForm*>	formMap;
+	std::string array[3] = {"robotomy request", "presidential pardon", "shrubbery creation"};
+	AForm	*formArray[3];
+	AForm	*result = NULL;
 
-	formMap["robotomy request"] = new RobotomyRequestForm(target);
-	formMap["presidential pardon"] = new PresidentialPardonForm(target);
-	formMap["shrubbery creation"] = new ShrubberyCreationForm(target);
-	if (formMap.find(formName) == formMap.end())
+	formArray[0] = new RobotomyRequestForm(target);
+	formArray[1] = new PresidentialPardonForm(target);
+	formArray[2] = new ShrubberyCreationForm(target);
+	for (int i = 0; i < 3; i++)
 	{
-		this->deleteForm(formMap, "<null>");
-		throw std::invalid_argument("Intern::FormDoesNotExist");
+		if (formName == array[i])
+		{
+			std::cout << "Intern creates " << formName << std::endl;
+			result = formArray[i];
+			break ;
+		}
 	}
-	AForm *result = (formMap[formName]);
-	this->deleteForm(formMap, formName);
-	std::cout << "Intern creates " << formName << std::endl;
+	this->deleteForm(formArray, formName);
+	if (result == NULL)
+		throw std::invalid_argument("Intern::FormDoesNotExist");
 	return (result);
 }
 
