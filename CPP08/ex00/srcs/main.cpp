f8b52bcfcfe37820ea682d7b9a96b04099958b4d
Cpapot <corentinpapot2004@gmail.com>
2024-02-05 13:52:19 +0100

    ok

--- CPP08/ex00/srcs/main.cpp
+++ CPP08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 14:32:30 by cpapot            #+#    #+#             */
-/*   Updated: 2023/12/22 14:01:30 by cpapot           ###   ########.fr       */
+/*   Updated: 2024/02/05 13:39:38 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,49 @@
 
 int main()
 {
-	std::vector<int> vector;
+	{
+		try
+		{
+			std::vector<int> vector;
 
-	for (int i = 0; i < 30; i++)
-		vector.push_back(i);
+		for (int i = 0; i < 30; i++)
+			vector.push_back(i);
 
-	easyfind(vector, 40);
+		easyfind(vector, 40);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+
+
+	{
+		std::vector<int> vector;
+
+		for (int i = 0; i < 30; i++)
+			vector.push_back(i);
+
+		easyfind(vector, 15);
+	}
+
+	{
+		std::vector<char> vector;
+		vector.push_back('a');
+		vector.push_back('b');
+		vector.push_back('c');
+		vector.push_back('d');
+		vector.push_back('e');
+		vector.push_back('f');
+		easyfind(vector, 'f');
+		try
+		{
+			easyfind(vector, 'w');
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+	}
 }
